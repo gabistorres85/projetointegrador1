6b809d5ab7e8e1d7eb230a15bbef4ae6a0f8af59
gabistorres <gabis.torres@gmail.com>
2025-09-20 17:43:34 -0300

    feat: mudança no MER

--- cursinho_OK/db.py
+++ cursinho_OK/db.py
@@ -2,8 +2,8 @@ import enum
 import hashlib
 import sqlite3
 from sqlite3.dbapi2 import Timestamp
-from sqlalchemy import ForeignKey, create_engine, Column, Integer, String, Date, DateTime, Enum
-from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy import ForeignKey, Table, create_engine, Column, Integer, String, Date, DateTime, Enum
+from sqlalchemy.orm import declarative_base, relationship, Mapped, mapped_column
 from sqlalchemy.sql import func
 
 # conecta ao banco de dados 'curso.db'
@@ -14,6 +14,12 @@ engine = create_engine("sqlite:///banco.db", echo=True)
 # Base para os modelos
 Base = declarative_base()
 
+curso_turma = Table(
+    "curso_turma",
+    Base.metadata,
+    Column("curso_id", Integer, ForeignKey("cursos.id_Curso"), primary_key=True),
+    Column("turma_id", Integer, ForeignKey("turmas.id_Turma"), primary_key=True)
+)
 # Criar tabelas hash
 def hash_senha(senha: str) -> str:
     return hashlib.sha256(senha.encode()).hexdigest()
@@ -42,24 +48,46 @@ class StatusPresenca(enum.Enum):
     falta = "falta"
     falta_justificada = "falta_justificada"
 
+class StatusArea(enum.Enum):
+    humanas = "humanas"
+    exatas = "exatas"
+    biologicas = "biologicas"
+
+class StatusTurno(enum.Enum):
+    matutino = "matutino"
+    vespertino = "vespertino"
+    noturno = "noturno"
+
+class Turma(Base):
+    __tablename__ = 'turmas'
+    id_Turma = Column(Integer, primary_key=True, autoincrement=True)
+    nome = Column(String, nullable=False, unique=True)
+    turmas = relationship("Cursos", secondary=curso_turma, back_populates="turmas") # Relação N:N
+    alunos = relationship("Aluno", backref="turma") # Relação 1:N
+
 
 class Genero(Base):
     __tablename__ = 'generos'
     id_Genero = Column(Integer, primary_key=True, autoincrement=True)
     descricao = Column(String, nullable=False, unique=True)
-    alunos = relationship("Aluno", back_populates="genero")
+    alunos = relationship("Aluno", backref="genero") #Relação N:1
+    professores = relationship("Professor", backref="genero") #Relação N:1
 
 class Pcd(Base):
     __tablename__ = 'pcds'
     id_Pcd = Column(Integer, primary_key=True, autoincrement=True)
     descricao = Column(String, nullable=False, unique=True)
-    alunos = relationship("Aluno", back_populates="pcd", uselist=False) # Relação um-para-um
+    alunos = relationship("Aluno", backref="genero") #Relação N:1
+    professores = relationship("Professor", backref="genero") #Relação N:1
+
+    
 
 class Raca(Base):
     __tablename__ = 'racas'
     id_Raca = Column(Integer, primary_key=True, autoincrement=True)
     descricao = Column(String, nullable=False, unique=True)
-    alunos = relationship("Aluno", back_populates="raca", uselist=False) # Relação um-para-um
+    alunos = relationship("Aluno", backref="genero") #Relação N:1
+    professores = relationship("Professor", backref="genero") #Relação N:1
 
 class Aluno(Base):
     __tablename__ = 'alunos'
@@ -67,31 +95,28 @@ class Aluno(Base):
     id_Genero = Column(Integer, ForeignKey('generos.id_Genero'))
     id_Pcd = Column(Integer, ForeignKey('pcds.id_Pcd'))
     id_Raca = Column(Integer, ForeignKey('racas.id_Raca'))
-    nomeAluno = Column(String, nullable=False)
+    id_Turma = Column(Integer, ForeignKey('turmas.id_Turma'))
+    nome = Column(String, nullable=False)
     datanasc = Column(String)
     cpf = Column(String(11))
     email = Column(String, nullable=False, unique=True)
     telefone = Column(String(11)) #11999999999
-    datamatricula = Column(Date)
-    dataconclusao = Column(Date)
-    observacoes = Column(String)
-    curso_id = Column(Integer)
     endereco = Column(String)
+    dt_matricula = Column(Date)
+    dt_conclusao = Column(Date)
+    observacoes = Column(String)
     presenca = Column(Enum(StatusPresenca), default=StatusPresenca.falta)
     e_faltoso = Column(Integer, default=0)
-    genero = relationship("Genero", back_populates="alunos")    
-    pcd = relationship("Pcd", back_populates="alunos", uselist=False) # Relação um-para-um
-    raca = relationship("Raca", back_populates="alunos", uselist=False) # Relação um-para-um
-    cursos = relationship("Turma", secondary="curso_aluno", back_populates="alunos") # Relação muitos-para-muitos
+    
 
 class Curso(Base):
     __tablename__ = 'cursos'
     id_Curso = Column(Integer, primary_key=True, autoincrement=True)
-    nome = Column(String, nullable=False, unique=True)
-    descricao = Column(String)
-    turno = Column(String)
     id_Professor = Column(Integer, ForeignKey('professores.id_Professor'))
-    professor = relationship("Professor", back_populates="cursos", uselist=False) # Relação um-para-um
+    nome = Column(String, nullable=False, unique=True)
+    area = Column(Enum(StatusArea), nullable=False)
+    turno = Column(Enum(StatusTurno))
+    professor = relationship("Professor", backref="cursos") # Relação N:1
 
 class Professor(Base):
     __tablename__ = 'professores'
@@ -103,108 +128,11 @@ class Professor(Base):
     cpf = Column(String(11))
     email = Column(String, nullable=False, unique=True)
     telefone = Column(String(11)) #11999999999
-    cursos = relationship("Curso", back_populates="professor") # Relação um-para-muitos
-    professores = relationship("Professor", back_populates="genero") 
-    pcd = relationship("Pcd", back_populates="alunos", uselist=False) # Relação n:1
-    raca = relationship("Raca", back_populates="alunos", uselist=False) # Relação m-para-um
-    genero = relationship("Genero", back_populates="professores")
+    endereco = Column(String)
 
-class Turma(Base):
-    __tablename__ = 'turmas'
-    id_Turma = Column(Integer, primary_key=True, autoincrement=True)
-    nomeTurma = Column(String, nullable=False, unique=True)
-    descricao = Column(String)
-    turno = Column(String)
-    curso_id = Column(Integer, ForeignKey('cursos.id_Curso'))
-    id_Aluno = Column(Integer, ForeignKey('alunos.id_Aluno'))
-    alunos = relationship("Aluno", secondary="curso_aluno", back_populates="cursos") #Relação muitos-para-muitos
-    curso = relationship("Curso", back_populates="turmas") # Relação um-para-muitosu
-
-class Matricula(Base):
-    __tablename__ = 'curso_aluno'
-    id_Matricula = Column(Integer, primary_key=True, autoincrement=True)
-    aluno_id = Column(Integer, ForeignKey('alunos.id_Aluno'))
-    turma_id = Column(Integer, ForeignKey('turmas.id_Turma'))
-    aluno = relationship("Aluno", back_populates="cursos", uselist=False) # Relação um-para-um
-    turma = relationship("Turma", back_populates="alunos") # Relação um-para-um
-    data_matricula = Column(DateTime)
 
 if __name__ == "__main__":
     Base.metadata.create_all(engine)
     print("Tabelas criadas com sucesso!")
-    
 
-'''def criar_tabelas_todo():
-    """ cria a tabela 'tarefa' caso ela não exista """
-    cursor = conn.cursor()
-    conn.execute("""
-    CREATE TABLE IF NOT EXISTS cursos (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        nome TEXT NOT NULL UNIQUE,
-        descricao TEXT,
-        turno TEXT
-        )
-    """)
 
-    conn.execute("""
-    CREATE TABLE IF NOT EXISTS alunos (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        nome TEXT NOT NULL,
-        datanasc DATE,
-        cpf CHAR(11),
-        email TEXT NOT NULL UNIQUE,
-        telefone TEXT,
-        datamatricula DATE,
-        dataconclusao DATE,
-        observacoes TEXT,
-        curso_id INTEGER,
-        endereco TEXT,
-        FOREIGN KEY (curso_id) REFERENCES cursos(id)
-        )
-    """)
-
-    conn.execute("""
-    CREATE TABLE IF NOT EXISTS usuarios (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        nome TEXT NOT NULL,
-        login TEXT NOT NULL UNIQUE,
-        senha_hash TEXT NOT NULL,
-        criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-        )
-    """)
-
-def add_aluno(aluno):
-    """ adiciona uma novo aluno """
-    print ("inserir")
-    print (aluno['nome'])
-    conn.execute("INSERT INTO alunos (nome, datanasc, cpf, email, telefone, datamatricula, dataconclusao,observacoes, curso_id, endereco) VALUES (?, ?, ?, ?, ?, ?, NULL, ?, ?, ?)", (aluno['nome'], aluno['datanasc'], aluno['cpf'], aluno['email'], aluno['telefone'], aluno['datamatricula'], aluno['observacoes'], aluno['curso_id'], aluno['endereco']))
-    conn.commit()
-
-def get_alunos(): # retorna um cursor
-    """ retorna a lista de alunos cadastras """
-    cursor = conn.execute("select id, nome, datanasc, cpf, email, telefone, datamatricula, dataconclusao, curso_id, endereco, observacoes from alunos")
-    return [dict(row) for row in cursor.fetchall()]
-
-def get_aluno_por_id(aluno_id):
-    """ Retorna os dados de um aluno específico pelo ID """
-    cursor = conn.execute("SELECT * FROM alunos WHERE id = ?", (aluno_id,))
-    result = cursor.fetchone()
-    return dict(result) if result else None
-
-def update_aluno(id, aluno):
-    """ Atualiza os dados de um aluno pelo ID """
-    conn.execute("""
-        UPDATE alunos
-        SET nome = ?, datanasc = ?, cpf = ?, email = ?, telefone = ?, datamatricula = ?, dataconclusao = ?, observacoes = ?, curso_id = ?, endereco = ?
-        WHERE id = ?
-    """, (aluno['nome'], aluno['datanasc'], aluno['cpf'], aluno['email'], aluno['telefone'], aluno['datamatricula'], aluno['dataconclusao'], aluno['observacoes'], aluno['curso_id'], aluno['endereco'], id))
-    conn.commit()
-
-def delete_aluno(id):
-    """ Remove um aluno pelo ID """
-    conn.execute("DELETE FROM alunos WHERE id = ?", (id,))
-    conn.commit()
-
-if __name__ == "__main__":
-    criar_tabelas_todo()
-    print("Tabelas criadas (se não existiam).")'''
